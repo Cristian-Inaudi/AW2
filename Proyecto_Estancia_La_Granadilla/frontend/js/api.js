0e4541375b35749dc6d56b02623a558cd5a7bfde
Cristián Inaudi <crinaudi@gmail.com>
2026-03-05 12:51:07 -0300

    Cambios en API_URL
    Para funcionamiento del back en Web Service

--- Proyecto_Estancia_La_Granadilla/frontend/js/api.js
+++ Proyecto_Estancia_La_Granadilla/frontend/js/api.js
@@ -1,4 +1,8 @@
-const API_URL = "http://localhost:5000";
+const API_URL =
+  window.location.hostname === "localhost" ||
+  window.location.hostname === "127.0.0.1"
+    ? "http://localhost:5000"
+    : "https://granadilla-api.onrender.com";
 
 // Obtener listado de habitaciones
 export async function getHabitaciones() {
